8303a5b0adcab291874dbe584c39100924b2216d
kamodev <bagu.dev@gmail.com>
2013-10-09 10:02:36 -0700

    Add jQuery version

---
+++ jQuery/QueryString.js
@@ -0,0 +1,10 @@
+(function ($) {
+        $.QueryValue = function(param) {
+            var URL = window.location.href;
+        };
+
+        $.QueryContains = function() {
+
+        };
+
+})(jQuery);
