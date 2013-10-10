c42c3c46dbd39c46481f54be962edef0d50bdea8
kamodev <bagu.dev@gmail.com>
2013-10-10 15:55:52 -0700

    Added more code...

--- jQuery/QueryString.js
+++ jQuery/QueryString.js
@@ -1,10 +1,47 @@
+/**
+ * The jQuery version of the QueryString object
+ * @param  {Object} $ The jQuery object
+ */
 (function ($) {
-        $.QueryValue = function(param) {
-            var URL = window.location.href;
-        };
 
-        $.QueryContains = function() {
+    // Object variables
+    var URL = window.location.href;
+    var urlParams = {};
 
-        };
+    // Checking for the query string
+    if (URL.indexOf('?') > 0) {
+        var URLQS = URL.substr(URL.indexOf('?') + 1);
+            URLQS = URLQS.replace(/\+/g, ' ');
+
+        if (URLQS != null || URLQS != undefined) {
+            var paramArr = URLQS.split('&');
+
+            for (var pidx = 0; pidx < paramArr.length; pidx++) {
+                var paramPair = paramArr[pidx].split('=');
+
+                if (paramPair.length == 2) {
+                    var key = decodeURIComponent(paramPair[0]);
+                    var val = decodeURIComponent(paramPair[1]);
+
+                    urlParams[key] = val;
+                }
+            }
+        }
+    }
+    /**
+     * This function checks to see if there is a query variable in the url
+     * @param {String} qskey The name of the query string parameter
+     */
+    $.QueryContains = function(qskey) {
+        return ((urlParams[key] !== null) && (urlParams[key] !== undefined));
+    };
+
+    /**
+     * Gets the value of the given query string parameter
+     * @param {String} param The name the query string parameter
+     */
+    $.QueryValue = function(param) {
+      return (((urlParams[key] !== null) && (urlParams[key] !== undefined)) ? urlParams[key] : false);
+    };
 
 })(jQuery);
